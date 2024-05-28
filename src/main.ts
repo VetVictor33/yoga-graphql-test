89f7a4c44a68e8329edb0c0d5176e31e1ad6c006
Victor Feliciano <victorjfeliciano@gmail.com>
2024-05-28 11:22:35 -0300

    feat: set graphql server and mongodb connection

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,20 @@
-console.log("Hello World!");
+import "dotenv/config";
+
+import { createServer } from "node:http";
+import mongoose from "mongoose";
+import { createYoga } from "graphql-yoga";
+
+import { schema } from "./schema";
+
+async function main() {
+  const yoga = createYoga({ schema });
+  const server = createServer(yoga);
+
+  await mongoose.connect(process.env.MONGO_URL!);
+
+  server.listen(4000, () => {
+    console.info("Server is running on http://localhost:4000/graphql");
+  });
+}
+
+main();
